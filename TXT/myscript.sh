cfb14fc29fc130b21382c123dfb782930490223e
Jeremiah Tennes <jeremiahtennes@gmail.com>
2020-10-12 09:15:30 +0700

    OS202 jeremiahtennes

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Cicak bin Kadal
+# Jeremiah Garcia Tennes
 # Mon Sep 28 22:03:27 WIB 2020
 # Tue 29 Sep 2020 11:02:39 AM WIB
 # PALSU BANGET SEKALI UBAH
 
-FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
 rm -f $SHA $SHA.asc
@@ -23,5 +23,5 @@ gpg --verify $SHA.asc $SHA
 
 exit 0
 
+
 # Mon Sep 28 21:05:04 WIB 2020
-© 2020 GitHub, Inc.
